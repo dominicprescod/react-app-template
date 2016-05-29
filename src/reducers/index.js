c350d4316b5df8ee554da4ac514779ad66decd55
Dominic Prescod <dominicprescod@gmail.com>
2016-05-29 09:39:10 -0400

    integrated redux

---
+++ src/reducers/index.js
@@ -0,0 +1,10 @@
+/* ./src/reducers/index.js */
+
+import { combineReducers } from 'redux';
+import MyApp from './MyApp';
+
+const rootReducer = combineReducers({
+  MyApp
+});
+
+export default rootReducer;
